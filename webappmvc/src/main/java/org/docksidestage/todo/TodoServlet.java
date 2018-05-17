6af7f9f4eb645229ae1fee7b863239521bb4296d
dasoran <dasoran@gmail.com>
2018-05-17 14:45:30 +0900

    View的なやつを分離した

--- webappmvc/src/main/java/org/docksidestage/todo/TodoServlet.java
+++ webappmvc/src/main/java/org/docksidestage/todo/TodoServlet.java
@@ -1,8 +1,6 @@
 package org.docksidestage.todo;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +10,6 @@ import org.docksidestage.core.DocksideServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author dasoran
- */
 public class TodoServlet extends DocksideServlet {
 
     private static final long serialVersionUID = 1L;
@@ -27,22 +22,8 @@ public class TodoServlet extends DocksideServlet {
         // DBとか永続化された情報アクセス
         TodoList todoList = TodoList.getInstance();
 
-        // ヘッダ系出力
-        resp.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\">");
-        out.println("<style>");
-        out.println("#todo-list li.completed label {");
-        out.println("color: #d9d9d9;");
-        out.println("text-decoration: line-through;");
-        out.println("}");
-        out.println("</style>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<div id=\"todoapp\">");
+        // 関連クラス初期化
+        TodoView todoView = new TodoView();
 
         // TODO追加！！
         String add = req.getParameter("add");
@@ -50,14 +31,6 @@ public class TodoServlet extends DocksideServlet {
             String value = req.getParameter("value");
             todoList.add(value);
         }
-        out.println("<header id=\"header\">");
-        out.println("<h1>todos</h1>");
-        out.println("<form action=\"/todo\" method=\"get\">");
-        out.println("<input type=\"hidden\" name=\"add\" value=\"true\">");
-        out.println("<input id=\"value\" name=\"value\" placeholder=\"What needs to be done?\" />");
-        out.println("<input type=\"submit\" value=\"送信\" />");
-        out.println("</form>");
-        out.println("</header>");
 
         // todoリストの状態変更
         String check = req.getParameter("check");
@@ -67,38 +40,19 @@ public class TodoServlet extends DocksideServlet {
             todoList.setCheck(checkId, toggledCheck);
         }
         String delete = req.getParameter("delete");
+        Boolean isDeleteError = false;
         if (delete != null && delete.equals("true")) {
             Integer deleteId = Integer.parseInt(req.getParameter("value"));
             Boolean nowCheck = todoList.get(deleteId).checked;
             if (nowCheck) {
                 todoList.delete(deleteId);
             } else {
-                out.println("<p>削除する前にチェックしてください</p>");
+                isDeleteError = true;
             }
         }
 
-        // todoリストを表示
-        out.println("<section id=\"main\">");
-        out.println("<ul id=\"todo-list\">");
-        List<TodoElement> existTodo = todoList.getAll();
-        for (int i = 0; i < existTodo.size(); i++) {
-            TodoElement todo = existTodo.get(i);
-            out.println("<li>");
-            if (todo.checked) {
-                out.println("<a href=\"/todo?check=true&value=" + i + "\">✓</a>");
-            } else {
-                out.println("<a href=\"/todo?check=true&value=" + i + "\">・</a>");
-            }
-            out.println("<label>" + todo.name + "</label>");
-            out.println("<a href=\"/todo?delete=true&value=" + i + "\">削除</a>");
-            out.println("</li>");
-        }
-        out.println("</ul>");
-        out.println("</section>");
-        out.println("</div>");
-        out.println("</body>");
-        out.println("</html>");
-
-        out.close();
+        // viewへ
+        todoView.renderTodoPage(resp, todoList.getAll(), isDeleteError);
     }
+
 }
---
+++ webappmvc/src/main/java/org/docksidestage/todo/TodoView.java
@@ -0,0 +1,73 @@
+package org.docksidestage.todo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+public class TodoView {
+
+    // staticにする必要あり？
+    public void renderTodoPage(HttpServletResponse resp, List<TodoElement> existTodo, Boolean isDeleteError)
+            throws ServletException, IOException {
+
+        // ヘッダ系出力
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\">");
+        out.println("<style>");
+        out.println("#todo-list li.completed label {");
+        out.println("color: #d9d9d9;");
+        out.println("text-decoration: line-through;");
+        out.println("}");
+        out.println("</style>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<div id=\"todoapp\">");
+
+        // TODO追加！！
+        out.println("<header id=\"header\">");
+        out.println("<h1>todos</h1>");
+        out.println("<form action=\"/todo\" method=\"get\">");
+        out.println("<input type=\"hidden\" name=\"add\" value=\"true\">");
+        out.println("<input id=\"value\" name=\"value\" placeholder=\"What needs to be done?\" />");
+        out.println("<input type=\"submit\" value=\"送信\" />");
+        out.println("</form>");
+        out.println("</header>");
+
+        // deleteのエラー
+        if (isDeleteError) {
+            out.println("<p>削除する前にチェックしてください</p>");
+        }
+
+        // todoリスト表示
+        out.println("<section id=\"main\">");
+        out.println("<ul id=\"todo-list\">");
+        for (int i = 0; i < existTodo.size(); i++) {
+            TodoElement todo = existTodo.get(i);
+            out.println("<li>");
+            if (todo.checked) {
+                out.println("<a href=\"/todo?check=true&value=" + i + "\">✓</a>");
+            } else {
+                out.println("<a href=\"/todo?check=true&value=" + i + "\">・</a>");
+            }
+            out.println("<label>" + todo.name + "</label>");
+            out.println("<a href=\"/todo?delete=true&value=" + i + "\">削除</a>");
+            out.println("</li>");
+        }
+
+        // フッター
+        out.println("</ul>");
+        out.println("</section>");
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+    }
+}
